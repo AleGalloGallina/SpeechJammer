4029d9b3d7add6b4cc990c20d3d3387b3883f39b
AleGalloGallina <agc5matel@gmail.com>
2025-11-13 20:33:49 +0100

    Interfaz pantalla principal, base

--- SpeechJammer/SpeechJammer/ContentView.swift
+++ SpeechJammer/SpeechJammer/ContentView.swift
@@ -8,12 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    let ligthOrange = Color(red: 255/255, green: 243/255, blue: 231/255)
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Ale!")
+            HStack {
+                Text("Speech Jammer")
+                    .font(.system(size: 22))
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding(.bottom, 45)
+                
+            Text("How much wood would a woodchuck chuck if a woodchuck would chuck wood?")
+                .font(.system(size: 36))
+                .fontWeight(.bold)
+            Spacer()
+            Button {
+                
+            } label: {
+                Image(systemName: "microphone.circle.fill")
+                    .font(.system(size: 118))
+                    .symbolRenderingMode(.palette)
+                    .foregroundStyle(.orange, ligthOrange)
+            }
+            Spacer()
+            ZStack {
+                Image("boca_cerrada")
+                    .resizable()
+                    .scaledToFit()
+                Button {
+                    
+                } label: {
+                    Text("New tongue twister")
+                        .font(.system(size: 22))
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                        .padding(.top, 215)
+                }
+                    
+            }
+           
         }
         .padding()
     }
