cb0c309bf9350abbff7d5a8fa6254c697a4e9344
AleGalloGallina <agc5matel@gmail.com>
2025-11-13 19:28:13 +0100

    initial commit

---
+++ SpeechJammer/SpeechJammer/SpeechJammerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechJammerApp.swift
+//  SpeechJammer
+//
+//  Created by Alejandra Gallo Cortés on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SpeechJammerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
