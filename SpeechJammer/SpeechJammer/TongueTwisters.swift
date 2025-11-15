9027648655c46c28ea09aeee27696b2bf5b459a9
AleGalloGallina <agc5matel@gmail.com>
2025-11-15 21:12:02 +0100

    Tongue twisters, todos lol

--- SpeechJammer/SpeechJammer/TongueTwisters.swift
+++ SpeechJammer/SpeechJammer/TongueTwisters.swift
@@ -8,7 +8,30 @@
 import Foundation
 
 let tongueTwisters: [String] = [
-    "Pepe pecas pica papas",
-    "Tres tristes tigres",
-    "El arsobizpo de Constantinopla quiere que..."
+    "She sells seashells by the seashore",
+    "Peter Piper picked a peck of pickled peppers",
+    "How can a clam cram in a clean cream can",
+    "I scream you scream we all scream for ice cream",
+    "Fuzzy Wuzzy was a bear Fuzzy Wuzzy had no hair",
+    "Betty Botter bought some butter but she said the butter’s bitter",
+    "A proper copper coffee pot",
+    "Six slippery snails slid slowly seaward",
+    "Unique New York Unique New York",
+    "Red lorry yellow lorry red lorry yellow lorry",
+    "A big black bug bit a big black bear",
+    "Fred fed Ted bread and Ted fed Fred bread",
+    "If two witches would watch two watches which witch would watch which watch",
+    "Toy boat toy boat toy boat",
+    "Which wristwatch is a Swiss wristwatch",
+    "Four furious friends fought for the phone",
+    "Green glass globes glow greenly",
+    "Brisk brave brigadiers brandished broad bright blades",
+    "Lesser leather never weathered wetter weather better",
+    "A snake sneaks to seek a snack",
+    "Can you can a can as a canner can can a can",
+    "The thirty three thieves thought they thrilled the throne",
+    "Thin sticks thick bricks",
+    "Eleven benevolent elephants",
+    "Crisp crusts crackle crunchily",
+    "How much wood would a woodchuck chuck if a woodchuck would chuck wood?"
 ]
